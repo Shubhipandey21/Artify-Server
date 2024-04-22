dffcfbaf6cf41a78683325a869ea3cbb20d391be
unknown <shubhijbm@gmail.com>
2024-04-22 16:22:16 +0530

    whitelist client

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const { Server } = require("socket.io");
 const cors = require('cors');
 
 const app = express();
-app.use(cors({origin: 'https://localhost:3001'}))
+const idDev = app.settings.env === 'development'
+const URL = isDev ? 'http://localhost:3001' : 'https://artify-vert.vercel.app/'
+app.use(cors({origin: URL}))
 const httpServer = createServer(app);
-const io = new Server(httpServer, { cors: 'https://localhost:3001' });
+const io = new Server(httpServer, { cors: URL });
 
 io.on("connection", (socket) => {
   console.log("server connected")
